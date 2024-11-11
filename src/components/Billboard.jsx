f544f9c8b3b7e8b74448b9fc58dc2c2d6261a0f5
Henry Acevedo <brunokane7@yahoo.com>
2024-11-10 19:54:33 -0500

    Mouseover zoom on billboard poster

--- src/components/Billboard.jsx
+++ src/components/Billboard.jsx
@@ -1,19 +1,7 @@
 import React from "react";
 import moviePoster from "../images/the-fly-movie-poster MV5BODcxMGMwOGEtMDUxMi00MzE5LTg4YTYtYjk1YjA4MzQxNTNlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg";
+import "./Billboard.css";
 // import { Movie } from "../Movie";
-
-const moviePosters = [];
-for (let i = 0; i < 7; i++) {
-  moviePosters.push(
-    <img
-      key={i}
-      src={moviePoster}
-      alt="movie poster"
-      height={300}
-      style={{ margin: 10 }}
-    ></img>
-  );
-}
 
 var movies = await fetch("https://localhost:7046/api/Movies");
 var moviesjson = await movies.json();
@@ -28,17 +16,74 @@ for (let i = 0; i < moviesjson.length; i++) {
   moviearray.push(<p key={i}>{moviesjson[i].title}</p>);
 }
 
+const moviePosters = [];
+for (let i = 0; i < moviesjson.length; i++) {
+  moviePosters.push(
+    <div>
+      <img
+        key={i}
+        src={moviePoster}
+        alt="movie poster"
+        height={300}
+        style={{ margin: 10 }}
+      ></img>
+      <p>Movie title</p>
+    </div>
+  );
+}
+
 const Billboard = () => {
   return (
     <div>
-      <div>
-        <img
-          src={moviePoster}
-          alt="movie poster"
-          height={300}
-          style={{ margin: 10 }}
-        ></img>
+    <button className="btn btn-danger">BOTON DE PRUEBA</button>
+      <div className="container">
+        <div className="row justify-content-start">
+          <div className="col-4">One of two columns</div>
+          <div className="col-4">One of two columns</div>
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-4">One of two columns</div>
+          <div className="col-4">One of two columns</div>
+        </div>
+        <div className="row justify-content-end">
+          <div className="col-4">One of two columns</div>
+          <div className="col-4">One of two columns</div>
+        </div>
+        <div className="row justify-content-around">
+          <div className="col-4">One of two columns</div>
+          <div className="col-4">One of two columns</div>
+        </div>
+        <div className="row justify-content-between">
+          <div className="col-4">One of two columns</div>
+          <div className="col-4">One of two columns</div>
+        </div>
+        <div className="row justify-content-evenly">
+          <div className="col-4">One of two columns</div>
+          <div className="col-4">One of two columns</div>
+        </div>
       </div>
+
+      <div className="container">
+        <div className="row">
+          <div>
+            <img
+              className="billboard-poster"
+              src={moviePoster}
+              alt="movie poster"
+            ></img>
+            <p>Movie title</p>
+          </div>
+          <div>
+            <img
+              className="billboard-poster"
+              src={moviePoster}
+              alt="movie poster"
+            ></img>
+            <p>Movie title</p>
+          </div>
+        </div>
+      </div>
+
       <div>{moviearray}</div>
       <div>{moviePosters}</div>
     </div>
