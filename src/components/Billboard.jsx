897ebf92bf10cba08dfb34db1b7aa27a356bf5b1
Henry Acevedo <brunokane7@yahoo.com>
2024-11-14 12:23:23 -0500

    Improved and cleaned movie array

--- src/components/Billboard.jsx
+++ src/components/Billboard.jsx
@@ -13,7 +13,17 @@ var moviesjson = await movies.json();
 // console.log(moviesjson[0].title);
 var moviearray = [];
 for (let i = 0; i < moviesjson.length; i++) {
-  moviearray.push(<p key={i}>{moviesjson[i].title}</p>);
+  // moviearray.push(<p key={i}>{moviesjson[i].title}</p>);
+  moviearray.push(
+    <div>
+      <img
+        className="billboard-poster"
+        src={moviePoster}
+        alt="movie poster"
+      ></img>
+      <p key={i}>{moviesjson[i].title}</p>
+    </div>
+  );
 }
 
 const moviePosters = [];
@@ -34,58 +44,18 @@ for (let i = 0; i < moviesjson.length; i++) {
 
 const Billboard = () => {
   return (
-    <div>
-    <button className="btn btn-danger">BOTON DE PRUEBA</button>
-      <div className="container">
-        <div className="row justify-content-start">
-          <div className="col-4">One of two columns</div>
-          <div className="col-4">One of two columns</div>
-        </div>
-        <div className="row justify-content-center">
-          <div className="col-4">One of two columns</div>
-          <div className="col-4">One of two columns</div>
-        </div>
-        <div className="row justify-content-end">
-          <div className="col-4">One of two columns</div>
-          <div className="col-4">One of two columns</div>
-        </div>
-        <div className="row justify-content-around">
-          <div className="col-4">One of two columns</div>
-          <div className="col-4">One of two columns</div>
-        </div>
-        <div className="row justify-content-between">
-          <div className="col-4">One of two columns</div>
-          <div className="col-4">One of two columns</div>
-        </div>
-        <div className="row justify-content-evenly">
-          <div className="col-4">One of two columns</div>
-          <div className="col-4">One of two columns</div>
-        </div>
-      </div>
-
-      <div className="container">
-        <div className="row">
-          <div>
-            <img
-              className="billboard-poster"
-              src={moviePoster}
-              alt="movie poster"
-            ></img>
-            <p>Movie title</p>
-          </div>
-          <div>
-            <img
-              className="billboard-poster"
-              src={moviePoster}
-              alt="movie poster"
-            ></img>
-            <p>Movie title</p>
-          </div>
-        </div>
-      </div>
+    <div className="container">
+      {/* <div>
+        <img
+          className="billboard-poster"
+          src={moviePoster}
+          alt="movie poster"
+        ></img>
+        <p>Movie title</p>
+      </div> */}
 
       <div>{moviearray}</div>
-      <div>{moviePosters}</div>
+      {/* <div>{moviePosters}</div> */}
     </div>
   );
 };
