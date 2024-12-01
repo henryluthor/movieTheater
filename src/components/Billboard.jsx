9e8c01c7823c5185a09c41fd66c519f34de89237
Henry Acevedo <brunokane7@yahoo.com>
2024-11-30 19:47:20 -0500

    Implemented function when clicking poster

--- src/components/Billboard.jsx
+++ src/components/Billboard.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import moviePoster from "../images/the-fly-movie-poster MV5BODcxMGMwOGEtMDUxMi00MzE5LTg4YTYtYjk1YjA4MzQxNTNlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg";
 import "./Billboard.css";
+import companyData from "../companyData.json";
 // import { Movie } from "../Movie";
-
-
 
 var movies = await fetch("https://localhost:7046/api/Movies");
 var moviesjson = await movies.json();
@@ -14,30 +13,45 @@ var moviesjson = await movies.json();
 // console.log("first movie title:");
 // console.log(moviesjson[0].title);
 var moviearray = [];
+console.log("estos son los id:");
 for (let i = 0; i < moviesjson.length; i++) {
+  console.log(moviesjson[i].id);
   moviearray.push(
     <div>
       <img
+        id={moviesjson[i].id}
         className="billboard-poster"
         src={moviePoster}
         alt="movie poster"
+        onClick={(event) => {
+          var posterClickedId = event.target.id;
+          GetMovieById(posterClickedId);
+        }}
       ></img>
       <p key={i}>{moviesjson[i].title}</p>
     </div>
   );
 }
 
-const Billboard = () => {  
+const Billboard = () => {
   return (
     <div className="container">
-      
       <div className="form">
-        <label htmlFor="movieId" className="form-label">Movie Id</label>
-        <input id="movieId" type="text" className="form-control"></input>
-        <button type="button" className="btn btn-primary" onClick={GetMovieById}>
+        <label htmlFor="movieId" className="form-label">
+          Movie Id
+        </label>
+        <input id="movieIdInput" type="text" className="form-control"></input>
+        
+        <button
+          type="button"
+          className="btn btn-success"
+          onClick={(event) => {
+            var x = parseInt(document.getElementById("movieIdInput").value);
+            GetMovieById(x);
+          }}
+        >
           Search
         </button>
-        <button type="button" className="btn btn-success" onClick={() => {GetMovieByIdV2(1)}}>Search v2</button>
       </div>
 
       <div>{moviearray}</div>
@@ -45,14 +59,20 @@ const Billboard = () => {
   );
 };
 
-function GetMovieById()
-{
-  console.log("This is the function GetMovieById")
-}
 
-const GetMovieByIdV2 = (id) => {
-  console.log("The id received by this function is:");
-  console.log(id);
+async function GetMovieById(id) {  
+  var movie = await fetch(companyData.API_URL + "/" + id);
+  var moviejson = await movie.json();
+  console.log("API response:");
+  if(moviejson.data == null)
+  {
+    console.log("The data in moviejson is null.");
+  }
+  else
+  {
+    console.log(moviejson.data);
+    console.log("movie id:");
+  }
 }
 
 export default Billboard;
