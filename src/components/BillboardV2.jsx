53bf3383d2d6b51477465ce8c7a91d614fc6e4c1
Henry Acevedo <brunokane7@yahoo.com>
2025-05-28 15:31:29 -0500

    Implemented showing loading button

--- src/components/BillboardV2.jsx
+++ src/components/BillboardV2.jsx
@@ -1,25 +1,54 @@
+import defaultPoster from "../images/default_poster.jpg";
+import companyData from "../companyData.json";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 
 const BillboardV2 = () => {
-  return (
-    <div>
-      <h2>THIS IS BILLBOARD V2</h2>
 
-      <button className="btn btn-success" type="button" disabled>
-        <span
-          className="spinner-border"
-          role="status"
-          aria-hidden="true"
-        ></span>
-        Loading...
-      </button>
+  const [movies, setMovies] = useState(null);
+  const [isLoading, setIsloading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("");
 
-      {/* Lone spinner */}
-      {/* <div class="spinner-border" role="status">
-        <span class="visually-hidden">Loading...</span>
-      </div> */}
+  useEffect( () => {
+    const fetchMovies = async () => {
+      try{
+        var movies = await fetch(companyData.API_URL);
+        var moviesJson = await movies.json();
+        console.log("response:");
+        console.log(moviesJson);
+        setMovies(moviesJson);
+      }
+      catch(error){
+        console.error(error.message);
+        setErrorMessage(error.message);
+      }
+      finally{
+        setIsloading(false);
+      }
+    }
 
-    </div>
+    fetchMovies();
+  }, []);
+
+  return(
+    <>
+    {errorMessage || isLoading ? (
+      <div>
+        {errorMessage || 
+        <div>
+          <button className="btn btn-primary" type="button" disabled>
+            <span className="spinner-border" aria-hidden="true"></span>
+            <span role="status">Loading...</span>
+          </button>
+        </div>}
+      </div>
+      ) : (
+      movies.map((movie) => (
+        <p>{movie.title}</p>
+      ))
+    )}
+    </>
   );
 };
 
